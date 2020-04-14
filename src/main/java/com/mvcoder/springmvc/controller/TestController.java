eea185a7527c66d893665832e2442ef58379dadd
mvcoder <1258235316@qq.com>
2020-04-14 18:28:27 +0800

    添加 RequestMapping 代码

--- src/main/java/com/mvcoder/springmvc/controller/TestController.java
+++ src/main/java/com/mvcoder/springmvc/controller/TestController.java
@@ -1,16 +1,32 @@
 package com.mvcoder.springmvc.controller;
 
+import cn.hutool.json.JSONUtil;
+import com.mvcoder.springmvc.bean.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.converter.json.GsonBuilderUtils;
 import org.springframework.lang.Nullable;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.servlet.mvc.Controller;
+
+import javax.servlet.http.HttpServletRequest;
+
 
 /**
  * Created by mvcoder on 2020/4/10.
  */
-public class TestController implements Controller {
+@Controller
+@RequestMapping("/test")
+public class TestController {
 
-    @Nullable
-    @Override
+    private User user;
+
+    @Autowired
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    @RequestMapping(value = "/test")
     public ModelAndView handleRequest(javax.servlet.http.HttpServletRequest httpServletRequest,
                                       javax.servlet.http.HttpServletResponse httpServletResponse) throws Exception {
         System.out.println("access TestController method!!");
@@ -20,4 +36,36 @@ public class TestController implements Controller {
         mav.setViewName("test");
         return mav;
     }
+
+    //ResponseBody 表示返回结果用json格式化，返回的内容样式为 application-json
+    //限制请求方法为 GET
+    @RequestMapping(value = "/json", method = RequestMethod.GET)
+    @ResponseBody
+    public User returnJsonTest(Integer id, String name, HttpServletRequest request){
+        System.out.println(id + ", " + name + " , " + request.getParameter("name"));
+        return user;
+    }
+
+    @RequestMapping(value = "/path/{p1}/{p2}")
+    @ResponseBody
+    public String testPath(@PathVariable int p1, @PathVariable String p2){
+        return "hello test path : " + p1 + " , " + p2;
+    }
+
+
+    //用 @RequestPara 注解设置默认值和参数的必须性
+    @RequestMapping(value = "/rpa")
+    @ResponseBody
+    public String testRequestParaAnnotation(@RequestParam(required = false, defaultValue = "1") int age){
+        return "age : " + age;
+    }
+
+    //可以自动将参数转成对象中的属性
+    @RequestMapping(value = "/entityParaConvert", method = RequestMethod.POST)
+    @ResponseBody
+    public String testEntityParaConvert(User user){
+        return JSONUtil.toJsonStr(user);
+    }
+
+
 }
